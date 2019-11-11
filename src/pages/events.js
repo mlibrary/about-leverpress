46dca45590fa52867b1a82f77933e97d881e622d
Seth A. Johnson <sethajoh@umich.edu>
2019-11-11 14:37:36 -0500

    fix events

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,7 +1,7 @@
 import React from "react"
 import {graphql} from "gatsby"
 import Layout from "../components/layout"
-import EventList from "../components/news/newsList"
+import EventList from "../components/events/eventList"
 
 const Events = ({data}) => {
   const events = data.allMarkdownRemark.edges
