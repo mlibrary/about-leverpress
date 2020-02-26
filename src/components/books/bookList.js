4861888172f296af604593b694f028cc80b12817
Seth A. Johnson <sethajoh@umich.edu>
2020-02-26 11:13:59 -0500

    Remove the BookCover component, we're only showing 2 BookCards on the front page.

--- src/components/books/bookList.js
+++ src/components/books/bookList.js
@@ -1,6 +1,5 @@
 import React from "react"
 import BookCard from "./bookCard"
-import BookCover from "./bookCover"
 
 const BookList = ({books}) => {
 
@@ -13,19 +12,6 @@ const BookList = ({books}) => {
             if (node.frontmatter.orderOnPage === 1 || node.frontmatter.orderOnPage === 2) {
               return (
                 <BookCard key={node.id} book={node.frontmatter} />
-              )
-            } else {
-              return null
-            }
-          })
-        }
-      </div>
-      <div className="row book-card-covers-only-list">
-        {
-          books.map(({node}) => {
-            if (node.frontmatter.orderOnPage > 5) {
-              return (
-                <BookCover key={node.id} book={node.frontmatter} />
               )
             } else {
               return null
