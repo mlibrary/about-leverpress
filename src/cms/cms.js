17c25a9dc9436e12ca5d1a8c03bce1be6f4d714c
Jonathan McGlone <jmcglone@umich.edu>
2021-03-05 14:55:26 -0500

    add link file button to cms

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,6 +3,8 @@ import LinkFileComponent from "./components/linkFileComponent"
 import LinkFileButtonComponent from "./components/linkFileButtonComponent"
 
 CMS.registerEditorComponent(LinkFileComponent);
+CMS.registerEditorComponent(LinkFileButtonComponent);
+
 
 // https://github.com/netlify/netlify-cms/issues/1737 and HELIO-3241
 window.CMS_MANUAL_INIT = true;
