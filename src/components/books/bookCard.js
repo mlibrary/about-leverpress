8a292b3098f5ff72797398aa9bac16b9b2b2e89a
Seth A. Johnson <sethajoh@umich.edu>
2025-06-26 14:00:18 -0400

    FULCRUMOPS-776 Update node to 16.20.0 and added a11y fixes.

--- src/components/books/bookCard.js
+++ src/components/books/bookCard.js
@@ -8,6 +8,7 @@ const Book = ({cover, book}) => {
     readLink,
     buyLink,
     description,
+    orderOnPage
   } = book
 
   const showBuyLink = buyLink === "noBuyLink" ? false : true 
@@ -20,11 +21,11 @@ const Book = ({cover, book}) => {
         </div>
         <div className="col-md-8">
           <div className="card-body">
-          <h3 className="card-title" dangerouslySetInnerHTML={{ __html: title }}></h3>
+          <h3 id={`book${orderOnPage}`}className="card-title" dangerouslySetInnerHTML={{ __html: title }}></h3>
             <p className="card-subtitle text-muted h6">{author}</p>
             <p className="card-text">{description}</p>
-            <a className="card-link btn btn-secondary btn-lg" role="button" href={readLink}>Read free online</a>
-            { showBuyLink === true && <a className="card-link btn btn-outline-primary btn-lg" role="button" href={buyLink}>Buy</a> }
+            <a className="card-link btn btn-secondary btn-lg" aria-describedby={`book${orderOnPage}`} href={readLink}>Read free online</a>
+            { showBuyLink === true && <a className="card-link btn btn-outline-primary btn-lg" aria-describedby={`book${orderOnPage}`} href={buyLink}>Buy</a> }
           </div>
         </div>
       </div>
